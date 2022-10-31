d3cfd703ed6695033e7c20b9dbc057fe8e736e9f
CodeMorecamp <71688722+CodeMorecamp@users.noreply.github.com>
2022-10-31 08:41:08 +0100

    Morr Works

---
+++ 7kyu.js
@@ -0,0 +1,11 @@
+// Return only even Number
+
+function noOdds( values ){
+    return values.filter(val => val % 2 === 0);
+}
+ 
+// Return only odd numbers
+
+function noEvens(values){
+    return values.filter(val => val % 2 === 1)
+}
